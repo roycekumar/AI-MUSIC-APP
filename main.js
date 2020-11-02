92c92781499dbf8789d4ecc76460262bc97ef64e
Royce Kumar <69853766+roycekumar@users.noreply.github.com>
2020-11-02 17:18:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song="";
 song2="";
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
 function preload(){
     song=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -9,7 +14,32 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
 }
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      scoreRightWrist =  results[0].pose.keypoints[10].score;
+      scoreLeftWrist =  results[0].pose.keypoints[9].score;
+      console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+      
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+  
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+          
+    }
+  }
